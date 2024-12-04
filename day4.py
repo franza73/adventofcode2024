1c9c3401502637be4178e4e2c38b442f6b55ba4a
Franciraldo Cavalcante <franza@Franciraldos-MacBook-Pro.local>
2024-12-04 08:26:51 -0700

    AoC day4 - refactor with new functions

--- day4.py
+++ day4.py
@@ -5,16 +5,42 @@ Advent of code day 4 - 2024
 from itertools import product
 
 
+DIRS = [[-1, -1], [-1, 0], [-1, 1],
+        [0, -1], [0, 1],
+        [1, -1], [1, 0], [1, 1]]
+XSET = set(['M', 'S'])
+
+
 def process(data_file):
     '''
-    Process
+    Process matrix to count x-mas measures by two methods
     '''
+    def _count_method_1():
+        cnt = 0
+        for d_x, d_y in DIRS:
+            x, y = first_x + d_x, first_y + d_y
+            i = 0
+            while 0 <= x < len_x and 0 <= y < len_y and i < 3 and \
+                    matrix[x][y] == 'MAS'[i]:
+                x += d_x
+                y += d_y
+                i += 1
+            if i == 3:
+                cnt += 1
+        return cnt
+
+    def _count_method_2():
+        cnt = 0
+        s1 = set([matrix[first_x-1][first_y-1],
+                  matrix[first_x+1][first_y+1]])
+        s2 = set([matrix[first_x-1][first_y+1],
+                  matrix[first_x+1][first_y-1]])
+        if s1 == XSET and s2 == XSET:
+            cnt += 1
+        return cnt
+
     total1 = total2 = 0
     matrix = []
-    dirs = [[-1, -1], [-1, 0], [-1, 1],
-            [0, -1], [0, 1],
-            [1, -1], [1, 0], [1, 1]]
-    s = set(['M', 'S'])
     with open(data_file, 'r', encoding="ascii") as file:
         for line in file.readlines():
             for line in line.splitlines():
@@ -22,24 +48,10 @@ def process(data_file):
     len_x, len_y = len(matrix), len(matrix[0])
     for first_x, first_y in product(range(len_x), range(len_y)):
         if matrix[first_x][first_y] == 'X':
-            for d_x, d_y in dirs:
-                x, y = first_x + d_x, first_y + d_y
-                i = 0
-                while 0 <= x < len_x and 0 <= y < len_y and i < 3 and \
-                        matrix[x][y] == 'MAS'[i]:
-                    x += d_x
-                    y += d_y
-                    i += 1
-                if i == 3:
-                    total1 += 1
+            total1 += _count_method_1()
         if 0 < first_x < len_x - 1 and 0 < first_y < len_y - 1 and \
                 matrix[first_x][first_y] == 'A':
-            s1 = set([matrix[first_x-1][first_y-1],
-                      matrix[first_x+1][first_y+1]])
-            s2 = set([matrix[first_x-1][first_y+1],
-                      matrix[first_x+1][first_y-1]])
-            if s1 == s and s2 == s:
-                total2 += 1
+            total2 += _count_method_2()
     return total1, total2
 
 
